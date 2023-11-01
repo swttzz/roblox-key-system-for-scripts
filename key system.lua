a1156d3f440b23b452ab2c8188950b3df4889f9d
Davi Andrade <149514841+swttzz@users.noreply.github.com>
2023-11-01 14:50:14 -0300

    Key System w/ rayfield

---
+++ key system.lua
@@ -0,0 +1,25 @@
+getgenv().SecureMode = true
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "swttzz hub",
+    LoadingTitle = "Script hub Loading!",
+    LoadingSubtitle = "by wtfdavi",
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = nil, 
+       FileName = "swetzhubconfig1"
+    },
+    
+    KeySystem = true, 
+    KeySettings = {
+       Title = "Check key bellow!",
+       Subtitle = "wtfdavi on discord",
+       Note = "Talk with script owner to get access!!",
+       FileName = "swttzzkey",
+       SaveKey = true,
+       GrabKeyFromSite = false,
+       Key = {"159753","tkngplays"} 
+    }
+ })
+
